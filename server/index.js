a85b5a37c5b42f3d570834aeea37aef4e6eed420
Sanyam Choudhary <iamsanyamchoudhary@gmail.com>
2024-10-30 20:33:28 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,17 +13,20 @@ dotenv.config()
 const port = process.env.PORT || 4000;
 // transfer fetched data from frontend in json format
 app.use(express.json())
-app.use(cors({
-origin:["http://localhost:3000"],
+app.use(cors(
+{
+origin:["https://deploy-mern-1whq.vercel.app"],
+methods:["POST","GET"],
 credentials:true
-}))
+}
+));
 app.use(cookieParser())
 app.use(AuthRouter)
 app.use(PetRouter)
 app.use(ProductRouter)
 app.use(OrderRouter)
 //connection can be done in seperate file but we are doinfg it here
-mongoose.connect('mongodb://127.0.0.1:27017/authentication')
+mongoose.connect('mongodb+srv://iamsanyamchoudhary:<fidofinder@123>@cluster0.4gwj4fk.mongodb.net/fidofinder?retryWrites=true&w=majority&appName=Cluster0')
 
 app.listen(port,()=>{
     console.log("Server is Running")
