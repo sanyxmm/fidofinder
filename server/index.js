dfa335585c091bd2c57e1212cc9afdc4ad887308
Sanyam Choudhary <iamsanyamchoudhary@gmail.com>
2024-10-30 22:39:26 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const port = process.env.PORT || 4000;
 app.use(express.json())
 app.use(cors(
 {
-origin:[""],
+origin:["https://fidofinder-frontend.vercel.app"],
 methods:["POST","GET"],
 credentials:true
 }
