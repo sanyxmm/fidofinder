27a54a4919128ee00704273ac4c2364b8a05e210
Sanyam Choudhary <iamsanyamchoudhary@gmail.com>
2024-11-02 13:15:00 +0530

    Update cartSlice.js

--- frontend/src/StateMangement/cartSlice.js
+++ frontend/src/StateMangement/cartSlice.js
@@ -7,7 +7,7 @@ const cartinitialState = {
     cartItems: [],
     shippingDetails:{},
     cartTotal:"",
-    url:"https://fidofinder-158yi5mh3-sanyams-projects-b97ce7f4.vercel.app/?vercelToolbarCode=4ucTwbrZ54iAIN0",
+    url:"https://fidofinder-158yi5mh3-sanyams-projects-b97ce7f4.vercel.app",
 };
 const cartSlice = createSlice({
     name: 'cart',
