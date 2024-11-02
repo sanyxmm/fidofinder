eb4b03f69f057f97f3d3549cff3ad768dec2d1e7
Sanyam Choudhary <iamsanyamchoudhary@gmail.com>
2024-11-02 11:30:38 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,33 +1,9 @@
 import express from 'express'
-import dotenv from 'dotenv'
 import mongoose from 'mongoose'
-// import cors from 'cors'
-// import cookieParser from 'cookie-parser'
-// import { AuthRouter } from './routes/auth.route.js'
-// import {PetRouter} from './routes/pet.route.js'
-// import { OrderRouter } from './routes/Order.route.js'
-// import { ProductRouter } from './routes/Product.route.js'
-import path from 'path';
 
-const app = express()   //creating app
-dotenv.config()
-const port = process.env.PORT || 4000;
-// transfer fetched data from frontend in json format
+const app = express()
+
 app.use(express.json())
-// app.use(cors(
-// {
-// origin:["https://fidofinder-frontend.vercel.app/FidoFinder"],
-// methods:["POST","GET","PATCH","PUT"],
-// credentials:true
-// }
-// ));
-// app.use(cookieParser())
-// app.use(AuthRouter)
-// app.use(PetRouter)
-// app.use(ProductRouter)
-// app.use(OrderRouter)
-
-
 
 //connection can be done in seperate file but we are doinfg it here
 mongoose.connect('mongodb+srv://iamsanyamchoudhary:<fidofinder@123>@cluster0.4gwj4fk.mongodb.net/fidofinder?retryWrites=true&w=majority&appName=Cluster0')
