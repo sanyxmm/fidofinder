e926cf54f9d7a67230cb310d2667dafb99e7471c
Sanyam Choudhary <iamsanyamchoudhary@gmail.com>
2024-11-02 12:00:04 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,8 +6,7 @@ const app = express()
 app.use(express.json())
 
 //connection can be done in seperate file but we are doinfg it here
-mongoose.connect('mongodb+srv://iamsanyamchoudhary:<fidofinder@123>@cluster0.4gwj4fk.mongodb.net/fidofinder?retryWrites=true&w=majority&appName=Cluster0')
-
+mongoose.connect('mongodb+srv://iamsanyamchoudhary:A5vIIggw3uTA83tz@fido.q9zue.mongodb.net/Ffido?retryWrites=true&w=majority&appName=fido')
 app.get("/",(req,res)=>{
   res.json("Hello")
 })
