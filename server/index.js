104dcd0ac9e5323e83700016991b86a8b181972d
Sanyam Choudhary <iamsanyamchoudhary@gmail.com>
2024-11-08 08:49:27 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,50 +1,30 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import mongoose from 'mongoose'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
 import { AuthRouter } from './routes/auth.route.js'
 import {PetRouter} from './routes/pet.route.js'
 import { OrderRouter } from './routes/Order.route.js'
 import { ProductRouter } from './routes/Product.route.js'
+import dbConnect from './db/dbconnect.js'
 
 const app = express()   //creating app
 dotenv.config()
 const port = process.env.PORT || 4000;
 // transfer fetched data from frontend in json format
-// const corsOptions = {
-//   origin: "https://fidofinder-frontend.vercel.app",  // Allowed origin
-//   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],  // Allowed methods, including OPTIONS
-//   allowedHeaders: ["Content-Type", "Authorization"],  // Explicitly allowed headers
-//   credentials: true  // Allow cookies if needed
-// };
-// app.use(cors({
-// origin:["http://localhost:3000"],
-// credentials:true
-// }))
-
-// app.options('*', cors(corsOptions));
 app.use(express.json())
+app.use(cors({
+origin:["http://localhost:3000"],
+credentials:true
+}))
 app.use(cookieParser())
-
 app.use(AuthRouter)
 app.use(PetRouter)
 app.use(ProductRouter)
 app.use(OrderRouter)
-
 //connection can be done in seperate file but we are doinfg it here
-mongoose.connect('mongodb+srv://iamsanyamchoudhary:A5vIIggw3uTA83tz@fido.q9zue.mongodb.net/Ffido?retryWrites=true&w=majority&appName=fido', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => {
-  console.log("Connected to MongoDB");
-}).catch((err) => {
-  console.error("Error connecting to MongoDB", err);
-});
+dbConnect();
 
-app.get("/",(req,res)=>{
-  res.json("Hello")
-})
 app.listen(port,()=>{
     console.log("Server is Running")
 })
